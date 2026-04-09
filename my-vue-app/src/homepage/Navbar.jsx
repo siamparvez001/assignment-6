7020105445688aaf278b5b7e399521671b4c2279
siamparvez001 <siamparvez003@gmail.com>
2026-04-10 00:13:30 +0600

    update navbar section

--- my-vue-app/src/homepage/Navbar.jsx
+++ my-vue-app/src/homepage/Navbar.jsx
@@ -1,37 +1,3 @@
-// import { useState } from "react";
-// import React, { useState } from "react";
-// import cardImg from "../assets/Icon (Stroke).png"
-// const [cartItems] = useState([]);
-// import React from "react";
-// const NavBar = () => {
-//     return (
-//         <div className="navbar max-w-10/12 mx-auto">
-//             <div className="navbar-start">
-//                 <div className="flex digitools items-center gap-1 font-bold text-xl "> <span className="">DigiTools</span> </div>
-//             </div>
-//             <div className="navbar-center hidden md:flex">
-//                 <ul className="menu menu-horizontal gap-10 px-1 text-lg">
-//                     <li><a className="text-black">Products</a></li>
-//                     <li><a className="text-black">Features</a></li>
-//                     <li><a className="text-black">Pricing</a></li>
-//                     <li><a className="text-black">Testimonials</a></li>
-//                     <li><a className="text-black">FAQ</a></li>
-//                 </ul>
-//             </div>
-//             <div className="navbar-end gap-5">
-//                 <div className="flex gap-2">
-//                     <img className="w-[20px] h-[20px]" src={cardImg} alt="" />
-                    
-//                     <a href="">Login</a>
-//                 </div>
-//                 <a className="btn bg-red-500 rounded-full text-white">Get Started</a>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default NavBar;
-
 
 import React from "react";
 import cardImg from "../assets/Icon (Stroke).png";
